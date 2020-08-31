e62b3e51ebf692bb8fba135d76ca3582d8b7b5b2
Daniel Maldonado <daniel.maldonado@bluealba.com>
2020-08-31 18:08:06 -0300

    cambio a espaniol las constanstes

--- Domain/Expresion.js
+++ Domain/Expresion.js
@@ -1,9 +1,9 @@
 const Literal = require("./Literal");
 
-const SUBTRACT_OPERATOR = "-"
-const ADITION_OPERATOR = "+"
-const MULTIPLY_OPERATOR = "*"
-const DIVISOR_OPERATOR = "/"
+const OPERADOR_RESTA = "-"
+const OPERADOR_SUMA = "+"
+const OPERADOR_MULTIPLICACION = "*"
+const OPERADOR_DIVISION = "/"
 
 const omit = {
   add(expresion) {
@@ -22,30 +22,30 @@ const omit = {
 
 class Expresion {
   static desde(expresion) {
-    if (expresion.includes(ADITION_OPERATOR)) {
+    if (expresion.includes(OPERADOR_SUMA)) {
       return expresion
-        .split(ADITION_OPERATOR)
+        .split(OPERADOR_SUMA)
         .reduce((expresion1, expresion2) => {
           return expresion1.add(Expresion.desde(expresion2))
         }, omit)
     }
-    if (expresion.includes(SUBTRACT_OPERATOR)) {
+    if (expresion.includes(OPERADOR_RESTA)) {
       return expresion
-        .split(SUBTRACT_OPERATOR)
+        .split(OPERADOR_RESTA)
         .reduce((expresion1, expresion2) => {
           return expresion1.subtract(Expresion.desde(expresion2))
         }, omit)
     }
-    if (expresion.includes(MULTIPLY_OPERATOR)) {
+    if (expresion.includes(OPERADOR_MULTIPLICACION)) {
       return expresion
-        .split(MULTIPLY_OPERATOR)
+        .split(OPERADOR_MULTIPLICACION)
         .reduce((expresion1, expresion2) => {
           return expresion1.multiply(Expresion.desde(expresion2))
         }, omit)
     }
-    if (expresion.includes(DIVISOR_OPERATOR)) {
+    if (expresion.includes(OPERADOR_DIVISION)) {
       return expresion
-        .split(DIVISOR_OPERATOR)
+        .split(OPERADOR_DIVISION)
         .reduce((expresion1, expresion2) => {
           return expresion1.divide(Expresion.desde(expresion2))
         }, omit)
